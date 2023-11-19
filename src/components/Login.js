2e7b662172ed0a188d3eb2c4a2076455743abec5
Ramjan Santi <69352293+urayan@users.noreply.github.com>
2023-11-19 11:17:35 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch(`http://localhost:5000/api/auth/login`, {
+            const response = await fetch(`http://51.20.133.230:5000/api/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
